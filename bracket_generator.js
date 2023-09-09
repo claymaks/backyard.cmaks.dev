f0a8ff56ee2ff9ad64a8d8306cad447a842c5eaa
Clay Maksymiuk <cmaks@umich.edu>
2023-09-09 07:18:07 -0700

    first commit

---
+++ bracket_generator.js
@@ -0,0 +1,207 @@
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex > 0) {
+
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+}
+
+function pair_name(name1, name2) {
+    return name1 + " & " + name2;
+}
+
+function generate_teams_of_two(players, preassigned) {
+    let team_list = preassigned;
+
+    for (let i = 0; i < preassigned.length; i++) {
+        let team = preassigned[i].split(" & ");
+        players.splice(players.indexOf(team[0]), 1);
+        players.splice(players.indexOf(team[1]), 1);
+    }
+
+    let shuffled_players = shuffle(players);
+    for (let i = 0; i < shuffled_players.length; i+=2) {
+        team_list.push(pair_name(shuffled_players[i], shuffled_players[i+1]));
+    }
+
+    return team_list;
+}
+
+const eqSet = (xs, ys) =>
+    xs.size === ys.size &&
+    [...xs].every((x) => ys.has(x));
+
+function Team(name) {
+    return {
+        name: name,
+        rounds_participated: new Set(),
+        opponents_played_against: new Set(),
+        games_played: new Set(),
+        my_games: {},
+    };
+}
+
+function check_valid_pair(team, slot_num, slot, game) {
+    if (team.rounds_participated.has(slot_num)) { return false; }
+    if (slot.length === 1 && team.opponents_played_against.has(slot[0].name)) { return false; }
+    if (slot.length === 1 && slot[0].name === team.name) { return false; }
+    if (slot.length === 2) { return false; }
+
+    team.rounds_participated.add(slot_num);
+    if (slot.length > 0) {
+        team.opponents_played_against.add(slot[0].name);
+        slot[0].opponents_played_against.add(team.name);
+    }
+    team.games_played.add(game);
+
+    team.my_games[slot_num] = {
+        game: game,
+        opponent: ""
+    };
+    console.log(team);
+    return true;
+}
+
+function undo_place(team, game, slot_num) {
+    team.rounds_participated.remove(slot_num);
+    team.games_played.remove(game);
+}
+
+function make_schedule(teams, num_games) {
+    let unscheduled = [];
+    for (let j = 0; j < num_games; j++) {
+        for (let team = 0; team < teams.length; team++) {
+            unscheduled.push([teams[team], j]);
+        }
+    }
+
+    console.log(unscheduled);
+
+    let schedule = {};
+    for (let i = 0; i < num_games; i++) {
+        schedule[i] = [];
+        for (let k = 0; k < Math.floor(unscheduled.length/2); k++) {
+            schedule[i].push([]);
+        }
+    }
+
+    for (let i = 0; i < unscheduled.length; i++) {
+        let team = unscheduled[i][0];
+        let game = unscheduled[i][1];
+
+        for (let k = 0; k < schedule[game].length; k++) {
+            if (!check_valid_pair(team, k, schedule[game][k], game)) {
+                continue;
+            }
+            if (schedule[game][k].length === 1) {
+                team.my_games[k].opponent = schedule[game][k][0].name;
+                schedule[game][k][0].my_games[k] = {
+                    opponent: team.name,
+                    game: game,
+                };
+            }
+            schedule[game][k].push(team);
+            break;
+        }
+    }
+
+    console.log(schedule);
+
+    for (let i = 0; i < schedule.length; i++) {
+        for (let k = 0; k < schedule[i].length; k++) {
+            if (schedule[i][k].length !== 1) { continue; }
+
+            let op = schedule[i][k].pop(0);
+            undo_place(op, i, k);
+
+            for (let k2 = 0; k < schedule[i].length; k2++) {
+                if (k === k2) { continue; }
+                op.my_games[k2].game = i;
+                if (! check_valid_pair(op, k2, schedule[game][k2], game)) { continue; }
+                schedule[i][k2].push(op);
+                break;
+            }
+        }
+    }
+
+    return {
+        schedule: schedule,
+        teams: teams
+    };
+}
+
+let test_string = "?player_names=Haley%0D%0AJaret%0D%0AClay%0D%0AAn%0D%0AAnna%0D%0AAlex&teammates=An+%26+Clay%0D%0AJaret+%26+Haley%0D%0A&num_games=2"
+function main() {
+    const queryString = window.location.search;
+    console.log(queryString);
+    const urlParams = new URLSearchParams(queryString);
+
+    let players = urlParams.get("player_names").split('\r\n').filter(n => n);
+    let preassigned = urlParams.get("teammates").split('\r\n').filter(n => n);
+    let num_games = parseInt(urlParams.get("num_games"));
+
+    console.log("Players: " + players);
+    console.log("Preassigned teammates: " + preassigned);
+    console.log("Num_games: " + num_games);
+
+    let raw_teams = generate_teams_of_two(players, preassigned);
+    console.log("Teams: " + raw_teams);
+    let teams = [];
+    for (let i = 0; i < raw_teams.length; i++) {
+        teams.push(Team(raw_teams[i]));
+    }
+
+    let schedule_and_teams = make_schedule(teams, num_games);
+    let schedule = schedule_and_teams.schedule;
+    let updated_teams = schedule_and_teams.teams;
+
+    let table = document.getElementById("bracket");
+    let thead = document.createElement("thead");
+    let trh = document.createElement("tr");
+    trh.appendChild(document.createElement("th"));
+    for (let i = 0; i < schedule[0].length; i++) {
+        let th = document.createElement("th");
+        th.innerText = "Round " + (i + 1);
+        trh.appendChild((th));
+    }
+    thead.appendChild(trh);
+    table.appendChild(thead);
+
+    let tbody = document.createElement("tbody");
+    console.log(updated_teams);
+    for (let i = 0; i < updated_teams.length; i++) {
+        let trb = document.createElement("tr");
+        let th = document.createElement("th");
+        th.innerText = updated_teams[i].name;
+        trb.appendChild(th);
+        for (let k = 0; k < schedule[0].length; k++) {
+            let td = document.createElement("td");
+            if (updated_teams[i].my_games[k] !== undefined) {
+                if (updated_teams[i].my_games[k].opponent) {
+                    td.innerText = "Playing game " + (updated_teams[i].my_games[k].game + 1) + " against " + updated_teams[i].my_games[k].opponent;
+                } else {
+                    td.innerText = "Playing game " + (updated_teams[i].my_games[k].game + 1) + ", but no opponent scheduled!";
+                    td.className += "red-highlight";
+                }
+            }
+            trb.appendChild(td);
+        }
+        tbody.appendChild(trb);
+    }
+    table.appendChild(tbody);
+
+    return schedule
+}
+
+let schedule = main();
+console.log(schedule);
---
+++ index.js
@@ -0,0 +1,267 @@
+// ----------------------------------------------------------------------------
+// Form manager.
+// ----------------------------------------------------------------------------
+
+let ON_TAB_LOAD = {
+    1: function() { load_players(); },
+    2: function() { sync_teammates(); }
+}
+
+let currentTab = 0; // Current tab is set to be the first tab (0)
+showTab(currentTab); // Display the current tab
+
+function showTab(n) {
+    // This function will display the specified tab of the form...
+    var x = document.getElementsByClassName("tab");
+    x[n].style.display = "block";
+    //... and fix the Previous/Next buttons:
+    if (n === 0) {
+        document.getElementById("prevBtn").style.display = "none";
+    } else {
+        document.getElementById("prevBtn").style.display = "inline";
+    }
+    if (n === (x.length - 1)) {
+        document.getElementById("nextBtn").innerHTML = "Submit";
+    } else {
+        document.getElementById("nextBtn").innerHTML = "Next";
+    }
+    //... and run a function that will display the correct step indicator:
+    fixStepIndicator(n)
+    if (ON_TAB_LOAD[n] !== undefined) { ON_TAB_LOAD[n]() }
+}
+
+function nextPrev(n) {
+    // This function will figure out which tab to display
+    var x = document.getElementsByClassName("tab");
+    // Exit the function if any field in the current tab is invalid:
+    if (n === 1 && !validateForm()) return false;
+    // Hide the current tab:
+    x[currentTab].style.display = "none";
+    // Increase or decrease the current tab by 1:
+    currentTab = currentTab + n;
+    // if you have reached the end of the form...
+    if (currentTab >= x.length) {
+        // ... the form gets submitted:
+        document.getElementById("regForm").submit();
+        return false;
+    }
+    // Otherwise, display the correct tab:
+    showTab(currentTab);
+}
+
+function validateForm() {
+    // This function deals with validation of the form fields
+    let x, y, i, valid = true;
+    x = document.getElementsByClassName("tab");
+    y = x[currentTab].getElementsByTagName("input");
+
+    if (document.getElementById("player_names").value.split('\n').filter(n => n).length % 2 === 1) {
+        valid = false;
+        document.getElementById("player_names").className = "invalid";
+    } else {
+        document.getElementById("player_names").className = "";
+    }
+    // A loop that checks every input field in the current tab:
+    // For backyard games, should only be 1.
+    for (i = 0; i < y.length; i++) {
+        // If a field is empty...
+        if (y[i].value === "") {
+            // add an "invalid" class to the field:
+            y[i].className += " invalid";
+            // and set the current valid status to false
+            valid = false;
+        }
+    }
+    // If the valid status is true, mark the step as finished and valid:
+    if (valid) {
+        document.getElementsByClassName("step")[currentTab].className += " finish";
+    }
+    return valid; // return the valid status
+}
+
+function fixStepIndicator(n) {
+    // This function removes the "active" class of all steps...
+    let i, x = document.getElementsByClassName("step");
+    for (i = 0; i < x.length; i++) {
+        x[i].className = x[i].className.replace(" active", "");
+    }
+    //... and adds the "active" class on the current step:
+    x[n].className += " active";
+}
+
+// ----------------------------------------------------------------------------
+// Manual team selection
+// ----------------------------------------------------------------------------
+function uuidv4() {
+    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+        (
+            c ^
+            (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+        ).toString(16)
+    );
+}
+
+function pair_name(pair) {
+    return pair.pair1.name + " & " + pair.pair2.name;
+}
+
+let players = [];
+let player_from_uuid = {};
+
+let pairs = [];
+let pair_from_uuid = {};
+
+let select_pairs_element = document.getElementById("pairs");
+let pillbox = document.getElementById("pillbox");
+let selected_tile = null;
+function load_players() {
+    let all_player_names = document.getElementById("player_names").value.split('\n').filter(n => n);
+    for (let i = 0; i < all_player_names.length; i++) {
+        // Skip names that already exist.
+        if (players.findIndex(player => {
+            return player.name === all_player_names[i];
+        }) !== -1) { continue; }
+
+        players.push({
+            name: all_player_names[i],
+            hidden: false,
+            uuid: uuidv4()
+        });
+        player_from_uuid[players[players.length-1].uuid] = players[players.length-1];
+    }
+
+    // Remove players that were removed from the list.
+    for (let i = 0; i < players.length; i++) {
+        // Skip names that match.
+        if (all_player_names.findIndex(player_name => {
+            return player_name === players[i].name;
+        }) !== -1) { continue; }
+
+        // Unlink pair if it exists.
+        if (players[i].pair_uuid && players[i].pair_uuid !== 0) {
+            let pair = pair_from_uuid[players[i].pair_uuid];
+
+            pair.hidden = true;
+            pair.pair1.hidden = false;
+            pair.pair1.pair_uuid = 0;
+            pair.pair2.hidden = false;
+            pair.pair2.pair_uuid = 0;
+        }
+
+
+        // Just clear and hide players that should be removed.
+        players[i].name = "";
+        players[i].hidden = true;
+    }
+
+    selected_tile = null
+    select_pairs(players);
+    populate_pills(pairs);
+}
+
+function sync_teammates() {
+    let input_field = document.getElementById("teammates");
+    input_field.value = "";
+    for (let i = 0; i < pairs.length; i++) {
+        if (pairs[i].hidden) { continue; }
+
+        input_field.value += pair_name(pairs[i]) + "\n";
+    }
+}
+
+function onTileClick(event) {
+    var tile = event.target;
+    if (selected_tile === null) {
+        selected_tile = tile;
+        tile.style.backgroundColor = "#85a2ff";
+    } else if (selected_tile === tile) {
+        selected_tile = null;
+        tile.style.backgroundColor = "#d2dcff";
+    } else {
+        var pair1 = player_from_uuid[tile.dataset.uuid];
+        var pair2 = player_from_uuid[selected_tile.dataset.uuid];
+
+        console.log("Pair created! " + pair1.name + " & " + pair2.name);
+
+        let pair_uuid = uuidv4();
+
+        pair1.hidden = true;
+        pair1.pair_uuid = pair_uuid;
+        pair2.hidden = true;
+        pair2.pair_uuid = pair_uuid;
+
+
+        var pair = {
+            pair1: pair1,
+            pair2: pair2,
+            pair_uuid: pair_uuid,
+            hidden: false
+        };
+
+        pairs.unshift(pair);
+        pair_from_uuid[pair_uuid] = pair;
+
+        selected_tile = null;
+
+        select_pairs(players);
+        populate_pills(pairs);
+    }
+}
+
+function onPillClick(event) {
+    var pill = event.target;
+    var pair = pair_from_uuid[pill.dataset.uuid];
+    if (
+        !window.confirm(
+            "Are you sure you want to break up the team " + pair_name(pair) + "?"
+        )
+    ) {
+        return;
+    }
+
+    pair.hidden = true;
+    pair.pair1.hidden = false;
+    pair.pair1.pair_uuid = 0;
+    pair.pair2.hidden = false;
+    pair.pair2.pair_uuid = 0;
+
+    select_pairs(players);
+    populate_pills(pairs);
+}
+
+function select_pairs(players) {
+    select_pairs_element.innerHTML = "";
+
+    for (var i = 0; i < players.length; i++) {
+        if (players[i].hidden) {
+            continue;
+        }
+        var tile = document.createElement("div");
+        tile.dataset.uuid = players[i].uuid;
+        tile.classList.add("tile");
+        var p = document.createElement("p");
+        p.dataset.uuid = players[i].uuid;
+        p.innerText = players[i].name;
+        tile.appendChild(p);
+        tile.addEventListener("click", onTileClick);
+        select_pairs_element.appendChild(tile);
+    }
+}
+
+function populate_pills(pairs) {
+    pillbox.innerHTML = "";
+    for (var i = 0; i < pairs.length; i++) {
+        if (pairs[i].hidden) {
+            continue;
+        }
+        var pill = document.createElement("div");
+        pill.dataset.uuid = pairs[i].pair_uuid;
+        pill.classList.add("pill");
+        var p = document.createElement("p");
+        p.dataset.uuid = pairs[i].pair_uuid;
+        p.innerText = pair_name(pairs[i]);
+        pill.appendChild(p);
+        pill.addEventListener("click", onPillClick);
+        pillbox.appendChild(pill);
+    }
+}
